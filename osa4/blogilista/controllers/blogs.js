096bd543aebc9248fcb990842fe5d1f4df3b285b
Olli Taka-Anttila <olli.takaanttila@gmail.com>
2021-12-04 19:47:18 +0200

    tehtavat 4.13-4.14

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -2,14 +2,16 @@ const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const logger = require('../utils/logger')
 
-
+// GET ALL
 blogsRouter.get('/', async (request, response) => {
-    logger.info("/api/blogs get request")
+    logger.info("get request")
     const blogsFound = await Blog.find({})
     response.json(blogsFound)
 })
 
+// POST
 blogsRouter.post('/', async (request, response, next) => {
+  logger.info("post request")
   if(!request.body.likes){
     request.body.likes = 0;
   }
@@ -18,9 +20,40 @@ blogsRouter.post('/', async (request, response, next) => {
   try {
     const result = await blog.save()
     response.status(201).json(result)
-  } catch(exception) {
-    next(exception)
+  } catch(e) {
+    next(e)
   }
 })
+
+// REMOVE ONE
+blogsRouter.delete('/:id', async (request, response, next) => {
+  logger.info("delete request")
+  try {
+    const deleted = await Blog.findByIdAndDelete(request.params.id)
+    if(deleted){
+      response.status(204).end()
+    } else {
+      logger.info("id not found - returning 400")
+      response.status(400).end()
+    }
+  } catch(e) {
+    next(e)
+  }
+})
+
+// CHANGE ONE
+blogsRouter.put('/:id', async (request, response, next) => {
+  logger.info("put request")
+  try {
+    const updatedBlog = 
+      await Blog.findByIdAndUpdate(request.params.id, request.body, { new: true})
+    response.json(updatedBlog)
+
+  } catch(e) {
+    next(e)
+  }
+})
+
+
 
 module.exports = blogsRouter
